88f5f09efed486ce01762d4b1c731c340a46b044
techquadradeveloper <techquadradeveloper@gmail.com>
2024-01-26 11:39:40 +0530

    Add SwitchSelector

--- src/Config/BaseUrl.js
+++ src/Config/BaseUrl.js
@@ -4,8 +4,8 @@ export const ZERODHA_URL="https://api.kite.trade/"
 // export const BASE_URL="http://localhost:5290/api/"
 // export const BASE_SIGNALR_HUB="http://localhost:5290/notification"
 //export const BASE_URL="https://localhost:7290/api/"
-export const BASE_URL="http://103.235.105.15:8081/api/"
-export const BASE_SIGNALR_HUB="http://103.235.105.15:8081/notification"
+export const BASE_URL="https://103.235.105.15:8082/api/"
+export const BASE_SIGNALR_HUB="https://103.235.105.15:8082/notification"
 
 
 
