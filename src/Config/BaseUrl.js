2b432a0c96dffe49542e2afca20a2bddc9f8a94a
techquadradeveloper <techquadradeveloper@gmail.com>
2024-03-12 11:46:08 +0530

    Some changes for base file

--- src/Config/BaseUrl.js
+++ src/Config/BaseUrl.js
@@ -1,11 +1,11 @@
 export const BROKER_URL="https://stock-api.fnotrader.com/v1/"
 export const FNOTRADER_URL="https://trade-api.fnotrader.com/v1/"
 export const ZERODHA_URL="https://api.kite.trade/"
-export const BASE_URL="http://localhost:5290/api/"
-export const BASE_SIGNALR_HUB="http://localhost:5290/notification"
+// export const BASE_URL="http://localhost:5290/api/"
+// export const BASE_SIGNALR_HUB="http://localhost:5290/notification"
 //export const BASE_URL="https://localhost:7290/api/"
-// export const BASE_URL="https://trade.fnotrader.com/api/"
-// export const BASE_SIGNALR_HUB="https://trade.fnotrader.com/notification"
+export const BASE_URL="https://trade.fnotrader.com/api/"
+export const BASE_SIGNALR_HUB="https://trade.fnotrader.com/notification"
 
 
 
