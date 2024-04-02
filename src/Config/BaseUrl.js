3dc0d81a7a16f5fd87ab9a537bedc6f9ec63ed32
techquadradeveloper <techquadradeveloper@gmail.com>
2024-04-02 15:13:22 +0530

    API url change

--- src/Config/BaseUrl.js
+++ src/Config/BaseUrl.js
@@ -1,4 +1,4 @@
-export const BROKER_URL="https://stock-api.fnotrader.com/v1/"
+export const BROKER_URL="https://stock-api2.fnotrader.com/v1/"
 export const FNOTRADER_URL="https://trade-api.fnotrader.com/v1/"
 export const ZERODHA_URL="https://api.kite.trade/"
 // export const BASE_URL="http://localhost:5290/api/"
